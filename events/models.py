24ae8d1f819a05977671877185c0644b06016138
Jod Alef <jod.alef@gmail.com>
2024-10-24 16:32:07 -0300

    Conserto de modelo, inserção de data_final

--- events/models.py
+++ events/models.py
@@ -5,8 +5,9 @@ from django.db import models
 class Evento(models.Model):
     nome_evento = models.CharField(max_length=100)
     data_evento = models.DateField()
+    data_final = models.DateField()
     descricao_evento = models.TextField()
     local_evento = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"Evento: {self.nome_evento} - Data: {self.data_evento} - Descricao: {self.descricao_evento} - Local: {self.local_evento}"
+        return f"Evento: {self.nome_evento} - Inicio: {self.data_evento} - Final: {self.data_final} - Descricao: {self.descricao_evento} - Local: {self.local_evento}"
